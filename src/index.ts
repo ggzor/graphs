27f91f0dc006e2e16a7e16a6c70b53b65a276503
ggzor <30713864+ggzor@users.noreply.github.com>
2018-07-13 21:16:13 -0500

    Updating example

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,28 @@
-import { of, fromEvent, NEVER } from 'rxjs'
-import { map, startWith, flatMap, delay, repeat, take, skip, tap } from 'rxjs/operators'
-import { Vertex } from './core/Vertex'
-import { Vector } from './visualization/geometry/Vector'
-import { sizes } from './visualization/manipulation/SizeObserver'
-import { createVertex } from './visualization/svg/vertex/VertexUtils'
 import * as SVG from 'svg.js'
-import { asRect } from './visualization/geometry/RectUtils'
-import { VertexState } from './visualization/svg/vertex/VertexVisual';
-import { delayItems } from './rx/AdditionalOperators';
+import { fromEvent, of, NEVER } from '../node_modules/rxjs';
+import { map, filter, tap } from '../node_modules/rxjs/operators';
+import { mouseEventAsVector } from './visualization/geometry/VectorUtils';
+import { createVertex } from './visualization/svg/vertex/VertexUtils';
+import { sizesOf } from './visualization/manipulation/SizeObserver';
+import { Vertex } from './core/Vertex';
+import { asRect } from './visualization/geometry/RectUtils';
 
 const canvas = SVG('root')
 const container = document.getElementById('container')
- 
-const vertices = of(2, 3, 4, 5, 30).pipe(
-    map(n => new Vertex("v1", n)),
-    delayItems(i => i * 2000)
-)
 
-const positions = fromEvent<MouseEvent>(container, 'click').pipe(
-    map(m => new Vector(m.offsetX, m.offsetY)),
-    startWith(new Vector(200, 200))
-)
+const containerBounds = sizesOf(container).pipe(map(asRect))
 
-const containerBounds = sizes(container).pipe(map(asRect))
-const canDrag = of(true)
-
-const states = of<VertexState>("selected")
-
-createVertex({ container: canvas, vertices, colors: NEVER, positions, canDrag, bounds: containerBounds, states })
+fromEvent<MouseEvent>(container, 'click').pipe(
+    map(mouseEventAsVector),
+).subscribe(v => {
+    createVertex({
+        container: container,
+        canvas: canvas,
+        bounds: containerBounds,
+        canDrag: of(true),
+        colors: NEVER,
+        positions: of(v),
+        states: NEVER,
+        vertices: of(new Vertex("v", 3))
+    })
+})
