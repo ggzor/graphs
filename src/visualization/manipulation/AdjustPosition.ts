5567152d73aa93382418003a78392c695533e12d
ggzor <30713864+ggzor@users.noreply.github.com>
2018-07-18 12:29:58 -0500

    Updating manipulation utils

--- src/visualization/manipulation/AdjustPosition.ts
+++ src/visualization/manipulation/AdjustPosition.ts
@@ -5,34 +5,44 @@ import { Rect } from "../geometry/Rect"
 import { Vector } from "../geometry/Vector"
 import { Size } from "../geometry/Size"
 
+import { VertexMovement } from "../svg/vertex/VertexPositioning"
+
 export function adjustToBounds(
-    positions: Observable<Vector>,
+    positions: Observable<VertexMovement>,
     sizes: Observable<Size>,
     bounds: Observable<Rect>,
-    positionIsFromCenter: boolean = false): Observable<Vector> {
+    positionIsFromCenter: boolean = false): Observable<VertexMovement> {
     return combineLatest(positions, sizes, bounds).pipe(
         map(t => {
-            const [position, size, bounds] = t
+            const [movement, size, bounds] = t
+            const position = movement instanceof Vector ? movement : movement[0]
+            const adjusted = adjustPosition(position, size, bounds, positionIsFromCenter)
 
-            const leftCorrection = positionIsFromCenter ? size.width / 2 : 0
-            const rightCorrection = positionIsFromCenter ? size.width / 2 : size.width
-
-            const topCorrection = positionIsFromCenter ? size.height / 2 : 0
-            const bottomCorrection = positionIsFromCenter ? size.height / 2 : size.height
-
-            const x = position.x - leftCorrection < bounds.left()
-                ? bounds.left() + leftCorrection
-                : position.x + rightCorrection > bounds.right()
-                    ? bounds.right() - rightCorrection
-                    : position.x
-
-            const y = position.y - topCorrection < bounds.top()
-                ? bounds.top() + topCorrection
-                : position.y + bottomCorrection > bounds.bottom()
-                    ? bounds.bottom() - bottomCorrection
-                    : position.y
-
-            return new Vector(x, y)
+            if (movement instanceof Vector)
+                return adjusted
+            else
+                return <VertexMovement>[adjusted, movement[1]]
         })
     )
+}
+
+function adjustPosition(position: Vector, size: Size, bounds: Rect, positionIsFromCenter: boolean) {
+    const leftCorrection = positionIsFromCenter ? size.width / 2 : 0
+    const rightCorrection = positionIsFromCenter ? size.width / 2 : size.width
+
+    const topCorrection = positionIsFromCenter ? size.height / 2 : 0
+    const bottomCorrection = positionIsFromCenter ? size.height / 2 : size.height
+
+    const x = position.x - leftCorrection < bounds.left()
+        ? bounds.left() + leftCorrection
+        : position.x + rightCorrection > bounds.right()
+            ? bounds.right() - rightCorrection
+            : position.x
+    const y = position.y - topCorrection < bounds.top()
+        ? bounds.top() + topCorrection
+        : position.y + bottomCorrection > bounds.bottom()
+            ? bounds.bottom() - bottomCorrection
+            : position.y
+
+    return new Vector(x, y)
 }
--- src/visualization/manipulation/Draggable.ts
+++ src/visualization/manipulation/Draggable.ts
@@ -1,22 +1,80 @@
-import { fromEvent } from "rxjs";
-import { map } from "rxjs/operators";
-
-import { mouseEventAsVector } from "../geometry/VectorUtils";
+import { fromEvent, merge } from "rxjs";
+import { map, throttleTime, tap } from "rxjs/operators";
 
 import { IVisual } from "../IVisual";
 import { IDraggable } from "./IDraggable";
+import { Vector } from "../geometry/Vector";
 
-export function fromVisual(container: HTMLElement, visual: IVisual): IDraggable {
+const getObservableFromEvent = <T extends Event>(mapper: (event: T) => Vector) =>
+    (container: HTMLElement) => (element: HTMLElement | Window) =>
+        (eventName: string, preventDefault: boolean = true) => {
+            return fromEvent<T>(element, eventName).pipe(
+                tap(event => {
+                    if (preventDefault)
+                        event.preventDefault()
+                }),
+                map(mapper),
+                map(v => {
+                    const bounds = container.getBoundingClientRect()
+                    return v.minus(new Vector(bounds.left, bounds.top))
+                })
+            )
+        }
+
+function mouseEventAsVector(event: MouseEvent): Vector {
+    const { clientX, clientY } = event
+    return new Vector(clientX, clientY)
+}
+
+function touchEventAsVector(event: TouchEvent): Vector {
+    const { clientX, clientY } = event.targetTouches.item(0)
+    return new Vector(clientX, clientY)
+}
+
+function adjustToDefaults(d: IDraggable, visual: IVisual): IDraggable {
     return {
-        start: fromEvent<MouseEvent>(visual.element, 'mousedown').pipe(
-            map(mouseEventAsVector),
-            map(v => v.minus(visual.getPosition()))
-        ),
-        move: fromEvent<MouseEvent>(container, 'mousemove').pipe(
-            map(mouseEventAsVector)
-        ),
-        end: fromEvent<MouseEvent>(window, 'mouseup').pipe(
-            map(mouseEventAsVector)
+        start: d.start.pipe(map(v => v.minus(visual.getPosition()))),
+        move: d.move.pipe(throttleTime(10)),
+        end: d.end
+    }
+}
+
+export function fromVisualWithMouse(container: HTMLElement, visual: IVisual): IDraggable {
+    const inContainer = getObservableFromEvent(mouseEventAsVector)(container)
+    const fromWindow = inContainer(window)
+
+    return adjustToDefaults({
+        start: inContainer(visual.element)("mousedown"),
+        move: fromWindow("mousemove"),
+        end: fromWindow("mouseup")
+    }, visual)
+}
+
+export function fromVisualWithTouch(container: HTMLElement, visual: IVisual): IDraggable {
+    const inContainer = getObservableFromEvent(touchEventAsVector)(container)
+    const fromWindow = inContainer(window)
+    const notMapped = getObservableFromEvent(() => Vector.zero)(container)(window)
+
+    return adjustToDefaults({
+        start: inContainer(visual.element)("touchstart"),
+        move: fromWindow("touchmove", false),
+        end: merge(
+            notMapped("touchend", false),
+            notMapped("touchcancel", false)
         )
-    };
+    }, visual)
+}
+
+function mergeDraggables(d1: IDraggable, d2: IDraggable): IDraggable {
+    return {
+        start: merge(d1.start, d2.start),
+        move: merge(d1.move, d2.move),
+        end: merge(d1.end, d2.end)
+    }
+}
+
+export function fromVisualWithMouseAndTouch(container: HTMLElement, visual: IVisual): IDraggable {
+    const funcs = [fromVisualWithMouse, fromVisualWithTouch]
+
+    return funcs.map(f => f(container, visual)).reduce(mergeDraggables)
 }
