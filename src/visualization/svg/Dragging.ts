b1ebed05e944ed17b06bfbb5350567d518013d5b
ggzor <30713864+ggzor@users.noreply.github.com>
2018-07-18 11:17:22 -0500

    Correcting positions from draggable pipeline

--- src/visualization/svg/Dragging.ts
+++ src/visualization/svg/Dragging.ts
@@ -1,30 +1,15 @@
-import { Observable, Timestamp, combineLatest } from "rxjs"
-import { timestamp, startWith, map } from "rxjs/operators"
-import { when } from "../../rx/AdditionalOperators";
+import { Observable } from "rxjs"
+import { map, windowToggle, withLatestFrom, flatMap } from "rxjs/operators"
 
 import { Vector } from "../geometry/Vector"
 
 import { IDraggable } from "../manipulation/IDraggable"
 
 export function positionsFrom(draggable: IDraggable): Observable<Vector> {
-    const startTimestamped = draggable.start.pipe(timestamp())
-    const endTimestamped = draggable.end.pipe(timestamp(), startWith<Timestamp<Vector>>(null))
-
-    const shouldMove = combineLatest(startTimestamped, endTimestamped)
-        .pipe(
-            map(l => {
-                const [press, release] = l
-
-                if (release != null)
-                    return press.timestamp > release.timestamp
-                else
-                    return press != null
-            }),
-            startWith(false)
-        )
-
-    return combineLatest(draggable.start, draggable.move).pipe(
-        when(shouldMove),
-        map(m => m[1].minus(m[0]))
+    return draggable.move.pipe(
+        withLatestFrom(draggable.start),
+        windowToggle(draggable.start, _ => draggable.end),
+        flatMap(w => w),
+        map(m => m[0].minus(m[1]))
     )
 }
