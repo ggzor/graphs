5df6e82fa4e9008aed39d6d39075e0bf6e30c469
ggzor <30713864+ggzor@users.noreply.github.com>
2018-07-18 12:30:08 -0500

    Updating test

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,32 @@
 import * as SVG from 'svg.js'
-import { fromEvent, of, NEVER } from '../node_modules/rxjs';
-import { map, filter, tap } from '../node_modules/rxjs/operators';
-import { mouseEventAsVector } from './visualization/geometry/VectorUtils';
-import { createVertex } from './visualization/svg/vertex/VertexUtils';
-import { sizesOf } from './visualization/manipulation/SizeObserver';
+import { of, NEVER, Observable } from 'rxjs';
 import { Vertex } from './core/Vertex';
-import { asRect } from './visualization/geometry/RectUtils';
+import { createVertex } from './visualization/svg/vertex/VertexBuilder';
+import { Vector } from './visualization/geometry/Vector';
+import { VertexMovement } from './visualization/svg/vertex/VertexPositioning';
+import { flatMap, scan, map, tap, startWith } from 'rxjs/operators';
+import { delayItems } from './rx/AdditionalOperators';
 
 const canvas = SVG('root')
 const container = document.getElementById('container')
 
-const containerBounds = sizesOf(container).pipe(map(asRect))
+const start = new Vector(0, 0)
+const nextVectors = [
+    new Vector(50, 50)
+]
 
-fromEvent<MouseEvent>(container, 'click').pipe(
-    map(mouseEventAsVector),
-).subscribe(v => {
-    createVertex({
-        container: container,
-        canvas: canvas,
-        bounds: containerBounds,
-        canDrag: of(true),
-        colors: NEVER,
-        positions: of(v),
-        states: NEVER,
-        vertices: of(new Vertex("v", 3))
-    })
+const positions = of(nextVectors).pipe(
+    flatMap(v => v),
+    scan((cur, next) => cur.add(next), start),
+    startWith(start),
+    delayItems(i => i * 2000),
+    map<Vector, VertexMovement>((v, i) => i == 0 ? v : [v, 0.4]),
+)
+
+const sub = createVertex(canvas, container, {
+    colors: NEVER,
+    vertices: of(new Vertex("V", 8)),
+    positions: positions
 })
+
+//setTimeout(() => sub.unsubscribe(), 10000)
